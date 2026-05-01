4f7bffef5d649e4aa1d3f6a4bc13b8c889b2f104
miku1001 <pauloted.22@gmail.com>
2026-05-01 16:53:35 +0800

    adding file uploading route

---
+++ backend/app/api/routes_upload.py
@@ -0,0 +1,43 @@
+import io
+import PyPDF2
+from fastapi import APIRouter, UploadFile, File, Form, HTTPException
+from app.services.rag_ingest import process_and_store_document
+
+router = APIRouter()
+
+@router.post("/api/upload")
+async def upload_document(company_id: str = Form(...), file: str = File(...)):
+  print(f"file accepted from {company_id}, {file.filename}")
+
+  extracted_text = ""
+
+  try:
+    if file.filename.endswith("pdf"):
+      # Basahin ang PDF
+      pdf_bytes = await file.read()
+      pdf_reader = PyPDF2.PdfReader(io.BytesIO(pdf_bytes))
+
+    elif file.filename.endswith(".txt"):
+          # Basahin ang normal na text file
+          text_bytes = await file.read()
+          extracted_text = text_bytes.decode("utf-8")
+          
+    else:
+        raise HTTPException(status_code=400, detail="PDF at TXT files lang ang pwede!")\
+    
+    if not extracted_text.strip():
+            raise HTTPException(status_code=400, detail="Walang text na nabasa sa file.")
+
+    print("Passing text for ingestion///")
+    result = process_and_store_document(company_id, extracted_text)
+
+    # success message
+    return {
+        "status": "success",
+        "filename": file.filename,
+        "message": result["message"]
+    }
+
+  except Exception as e:
+        print(f"May error: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
